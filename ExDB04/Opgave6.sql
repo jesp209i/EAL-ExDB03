89d37b3ce2497a468c9cb557958600df3346bbbe
jesp209i <jesp209i@edu.eal.dk>
2017-11-20 11:41:51 +0100

    Opgave6
    opgaver til onsdag skrevet ind

---
+++ ExDB04/Opgave6.sql
@@ -0,0 +1,25 @@
+/*3.35
+Display the last name, first name, and email of any owners of cats. Use subquery.*/
+
+
+/*3.36
+Display the last name, first name, and email of any owners if cats with the name Teddy. Use subquery.*/
+
+/*3.37
+Write an SQL statement to (1) create the BREED table, (2) insert the data in figure 3-21 into the BREED table, 
+(3) alter the PET_3 table so that PetBreed is a foreign key referencing BreedName in BREED, and 
+(4) With the BREED table added to the pet database, write an SQL statement to display the last name, first name,
+and email of any owner of a pet that has an AverageLifeExpectancy value greater than 15. Use subquery.*/
+
+/*  3.38
+Answer question 3.35, but ue a joins using JOIN ON syntax*/
+
+/*3.39
+Answer question 3.36, but use a joins using JOIN ON syntax*/
+
+/*3.40
+Answer part (4) ofquestion 3.37, but use joins using JOIN ON syntax*/
+
+/*3.41
+Display the OwnerLastName, OwnerFirstName, PetName, PetType, PetBreed, and AverageLifeExpectancy for pets with
+a known PetBreed*/
